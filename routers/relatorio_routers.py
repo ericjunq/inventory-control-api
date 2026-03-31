d0e63c54b9e388f62e326708a0fa055ccb16e571
Eric Junqueira <junqueiraeric576@gmail.com>
2026-03-31 04:06:16 -0300

    a

--- routers/relatorio_routers.py
+++ routers/relatorio_routers.py
@@ -7,6 +7,7 @@ from models import Usuario, Produto, Movimentacoes
 from fastapi.responses import FileResponse
 from enums import OrdemEnum, PeriodoEnum
 from sqlalchemy import func
+from datetime import datetime, timezone, timedelta
 
 relatorio_router = APIRouter(prefix='/relatorio', tags=['relatorio'])
 
@@ -87,7 +88,6 @@ async def ordenar_produtos(
         } for p in produtos
     ])
 
-    # verifica se o dicionário está vazio, já que o groupby que abaixo quebraria o codigo
     if produtos_df.empty:
         return {}
     
@@ -112,13 +112,69 @@ async def ordenar_produtos(
     # retorna os produtos ordenados de acordo o filtro das suas movimentações
     return produtos_df.to_dict('records')
 
+
 @relatorio_router.get('/ordenar_periodo')
 async def ordenar_periodo(
     exportar: bool = False,
     db: Session = Depends(get_db),
     usuario: Usuario = Depends(get_current_user),
     periodo: PeriodoEnum = PeriodoEnum.mes
-):
-    produto = db.query()
+):  
+    agora = datetime.now(timezone.utc)
+    if periodo == PeriodoEnum.dia:
+        data_inicial = agora - timedelta(days=1)
+    elif periodo == PeriodoEnum.semana:
+        data_inicial = agora - timedelta(weeks=1)
+    elif periodo == PeriodoEnum.mes:
+        data_inicial = agora - timedelta(days=30)
+    elif periodo == PeriodoEnum.trimestre:
+        data_inicial = agora - timedelta(days=90)
+    elif periodo == PeriodoEnum.semestre:
+        data_inicial = agora - timedelta(days=180)
+    elif periodo == PeriodoEnum.ano:
+        data_inicial = agora - timedelta(days=365)
+    
+    produtos = db.query(
+        # Colunas do banco
+        Produto.id,
+        Produto.sku,
+        Produto.nome,
+        Produto.categoria,
+        Movimentacoes.tipo,
+        Movimentacoes.quantidade,
+        Movimentacoes.observacao,
+        Movimentacoes.created_at
+    ).join(
+        Produto, Produto.id == Movimentacoes.produto_id # União de tabelas 
+    ).filter(
+        Produto.usuario_id == usuario.id, # verifica que cada usuario tem acesso aos próprios produtos
+        Movimentacoes.created_at >= data_inicial # filtra periodo selecionado
+    ).all()
 
+    # criação de dataframe pra resposta padronizada
+    produtos_df = pd.DataFrame([
+        {
+            'id': p.id,
+            'codigo_produto': p.sku,
+            'nome': p.nome,
+            'categoria': p.categoria.value,
+            'movimentacao': p.tipo.value,
+            'quantidade': p.quantidade,
+            'criado_em': p.created_at
+        } for p in produtos
+    ])
 
+    # verifica se o dicionário ta vazio
+    if produtos_df.empty:
+        return {}
+    
+    # alteração do nome de arquivo (desnecessário, só por que eu quis)
+    if exportar:
+        produtos_df.to_excel(f'relatorio_{periodo.value}.xlsx')
+        return FileResponse(
+            f'relatorio_{periodo.value}.xlsx',
+            filename=f'relatorio_{periodo.value}.xlsx',
+            media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+    
+    return produtos_df.to_dict('records')
